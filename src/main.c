38995292f7c44b89ccadcd0936e11d5bcdd2c6bb
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-05-19 15:48:56 -0500

    feat: Add function to detect if a world is full
    This will be useful for stopping an experiment when the seed has been
    incremented to the end and all possible states have been exhausted in
    the state space.

--- src/main.c
+++ src/main.c
@@ -170,6 +170,20 @@ void world_increment_seed(struct World *world) {
   }
 }
 
+int world_full(struct World *world) {
+  struct Vec2i dimensions = world_get_dimensions(world);
+
+  for (int y = 0; y < dimensions.y; y++) {
+    for (int x = 0; x < dimensions.x; x++) {
+      if (world_get_square(world, x, y).value == ctx.cell_dead) {
+        return 0;
+      }
+    }
+  }
+
+  return 1;
+}
+
 int main(int argc, char *argv[]) {
   ctx.cell_alive = 'O';
   ctx.cell_dead = ' ';
